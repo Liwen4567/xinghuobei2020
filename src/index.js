dd5d0f63608ae623f685057d3d49932f8691f621
helei <1797634191@qq.com>
2020-04-03 15:03:35 +0800

    part4

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Home from './pages/part4';
+
+ReactDOM.render(<Home/>, document.getElementById('root'));
